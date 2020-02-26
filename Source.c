9491b89a23af6e031de775a9a3b7f063214a2de9
Stephen Read <swread2005@hotmail.com>
2020-02-26 11:24:20 -0800

    Add project files.

---
+++ Source.c
@@ -0,0 +1,88 @@
+// Assignment 6
+
+ 
+/* #include <stdio.h>
+
+void main() {
+    int count = 1;
+    int prod =0;
+
+    while (prod < 56) {
+        prod = count * 8;
+        printf("%d, ", prod);
+        count++;
+    }
+} 
+*/
+
+
+/* #include <stdio.h>
+
+void main() {
+    int count = 1;
+    int prod = 0;
+
+    while (prod <= 139) {
+        prod = count * (5*7);
+        printf("%d, ", prod);
+        count++;
+    }
+}
+*/
+
+
+/* #include <stdio.h>
+
+void main() {
+
+    int sum = 0;
+    int count = 1;
+    int prod = 0;
+
+    While (sum <= 400) {
+        prod = count * 6;
+        sum = prod + sum;
+        count++;
+    }
+    Printf("The sum is %d", sum);
+   
+}
+*/
+
+
+/*
+Int sum, prod = 0;
+Int count = 17;
+
+              While (sum <= 400) {
+                           prod = count * 6;
+                           sum = prod + sum;
+count = count +1;
+              }
+              Printf (sum, “\n,”);
+
+
+
+*/
+
+/* #include <stdio.h>
+
+void main() {
+
+    int incr = 2;
+    int sum = 0;
+    int count = 51;
+
+    while (count <= 200) {
+
+        sum = count + sum;
+        count += incr;
+    }
+    printf ("Sum = %d ", sum);
+
+}
+*/
+
+
+
+
